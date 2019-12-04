aec59bd3a9b7d0f9e58d1a0d8aa0cd61b88e6886
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-04 14:49:32 -0600

    All GET requests working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const postDB = require('./posts/postDb')
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+  console.log()
 });
 
 //middleware
@@ -13,6 +14,54 @@ server.use(logger);
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  next();
+}
+
+//endpoints
+
+server.get('/api/users', (req, res) => {
+  userDB.get(req.query)
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(400).json(error)
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  userDB.getById(req.params.id)
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(400).json(error)
+    })
+})
+
+server.get('/api/posts', (req, res) => {
+  postDB.get(req.query)
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(400).json(error)
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  postDB.getById(req.params.id)
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(400).json(error)
+    })
+})
 
 module.exports = server;
