9e21cf395f38ba21552d1e5aed956a93b345cea5
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-04 14:18:19 -0600

    basic server set up

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
 const server = express();
+const userDB = require('./users/userDb')
+const postDB = require('./posts/postDb')
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+//middleware
+server.use(express.json());
+server.use(logger);
 
 //custom middleware
 
