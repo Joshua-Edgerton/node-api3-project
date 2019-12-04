3892f6dd5113a6b865a9bd187c49530c9511a493
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-04 17:11:59 -0600

    MVP met, all middleware validations working

--- server.js
+++ server.js
@@ -9,17 +9,61 @@ server.get('/', (req, res) => {
 
 //middleware
 server.use(express.json());
-server.use(logger);
-
-//custom middleware
 
 function logger(req, res, next) {
+  console.log(req.method, req.url, `${new Date().toISOString()}`)
   next();
+}
+
+function validateUserId(req, res, next) {
+  const { id } = req.params;
+
+  userDB
+    .getById(id)
+    .then(userId => {
+      if (userId) {
+        userId = req.user;
+        next();
+      } else {
+        res.status(400).json({ errorMessage: "Invalid user id" });
+      }
+    })
+    .catch(error => {
+      console.log(
+        res
+          .status(500)
+          .json({ error: "Error validating the user id" })
+      );
+    });
+}
+
+function validateUser(req, res, next) {
+  const user = req.body;
+
+  if (!user) {
+    res.status(400).json({ message: "Missing user data" });
+  } else if (!user.text) {
+    res.status(400).json({ message: "Missing required text field" });
+  } else {
+    next();
+  }
+}
+
+function validatePost(req, res, next) {
+  const posts = req.body;
+
+  if (!posts.user_id) {
+    res.status(400).json({ message: "Missing user data" });
+  } else if (!posts.text) {
+    res.status(400).json({ message: "Missing required text field" });
+  } else {
+    next();
+  }
 }
 
 //endpoints
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', logger, (req, res) => {
   userDB.get(req.query)
     .then(users => {
       res.status(200).json(users)
@@ -29,7 +73,7 @@ server.get('/api/users', (req, res) => {
     })
 })
 
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/users/:id', logger, validateUserId, (req, res) => {
   userDB.getById(req.params.id)
     .then(users => {
       res.status(200).json(users)
@@ -39,7 +83,7 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
-server.get('/api/posts', (req, res) => {
+server.get('/api/posts', logger, (req, res) => {
   postDB.get(req.query)
     .then(users => {
       res.status(200).json(users)
@@ -49,7 +93,7 @@ server.get('/api/posts', (req, res) => {
     })
 })
 
-server.get('/api/posts/:id', (req, res) => {
+server.get('/api/posts/:id', logger, validateUserId, (req, res) => {
   postDB.getById(req.params.id)
     .then(users => {
       res.status(200).json(users)
@@ -59,21 +103,29 @@ server.get('/api/posts/:id', (req, res) => {
     })
 })
 
-server.post('/api/posts/:id', (req, res) => {
+server.post('/api/posts/:id', logger, validatePost, (req, res) => {
   if (req.params.id) {
-    console.log(req.body)
     postDB.insert(req.body)
     .then(response => {
-      console.log(response)
+      res.status(200).json({ message: "Succesfully posted", response })
+    })
+    .catch(error => {
+      res.status(400).json({ message: "invalid ID" })
+    })
+} else {
+  res.status(400).json({ message: "Error posting text" })
+}
+})
+
+server.post('/api/users/', logger, validateUser, (req, res) => {
+
+    postDB.insert(req.body)
+    .then(response => {
       res.status(200).json({ message: "Succesfully posted" })
     })
     .catch(error => {
-      res.status(400).json(error)
+      res.status(400).json({ message: "error posting text" })
     })
-} else {
-  res.status(400).json({ message: "error posting text" })
-}
-
 })
 
 module.exports = server;
